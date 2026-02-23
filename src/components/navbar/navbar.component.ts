45bbd92418facb58bf8239868acd72a1aebe7b6a
Akram <akrambenaoun1993@gmail.com>
2026-02-24 00:06:16 +0100

    add new scroll function

--- src/components/navbar/navbar.component.ts
+++ src/components/navbar/navbar.component.ts
@@ -5,11 +5,18 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
   standalone: true,
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class NavbarComponent {
   mobileOpen = false;
 
+  scrollTo(sectionId: string) {
+    const el = document.getElementById(sectionId);
+    if (el) {
+      el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
+  
   toggleMenu() {
     this.mobileOpen = !this.mobileOpen;
   }
