53cf51019dbb2e6dd0673fb0b02c84cedf86da08
Akram <akrambenaoun1993@gmail.com>
2026-02-24 00:07:28 +0100

    add a shrinking navbar animation

--- src/components/navbar/navbar.component.ts
+++ src/components/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { HostListener } from '@angular/core';
 
+@HostListener('window:scroll', [])
 @Component({
   selector: 'app-navbar',
   standalone: true,
@@ -10,13 +12,24 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 export class NavbarComponent {
   mobileOpen = false;
 
+  onWindowScroll() {
+    const scrollTop = window.scrollY;
+    const navbar = document.querySelector('.navbar') as HTMLElement;
+
+    if (scrollTop > 50) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  }
+  
   scrollTo(sectionId: string) {
     const el = document.getElementById(sectionId);
     if (el) {
       el.scrollIntoView({ behavior: 'smooth', block: 'start' });
     }
   }
-  
+
   toggleMenu() {
     this.mobileOpen = !this.mobileOpen;
   }
