1212c0f2c4bc3e55b02a1ce0bcd52402912b1890
Akram <akrambenaoun1993@gmail.com>
2026-02-25 00:06:21 +0100

    update about-me components + other changes

--- src/components/about-me/about-me.component.ts
+++ src/components/about-me/about-me.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about-me',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './about-me.component.html',
-  styleUrl: './about-me.component.css'
+  styleUrls: ['./about-me.component.css'],
 })
-export class AboutMeComponent {
+export class AboutMeComponent implements OnInit {
+  loading = true;
+  fullText = 'Full-Stack JavaScript Developer';
+  typedText = '';
+  typingSpeed = 60;
 
+  ngOnInit() {
+    this.loading = false;
+    this.startTyping();
+
+    // setTimeout(() => {
+    //   this.loading = false;
+    //   this.startTyping();
+    // }, 1200);
+  }
+
+  startTyping() {
+    let index = 0;
+    const interval = setInterval(() => {
+      this.typedText += this.fullText[index];
+      index++;
+      if (index === this.fullText.length) {
+        clearInterval(interval);
+      }
+    }, this.typingSpeed);
+  }
 }
