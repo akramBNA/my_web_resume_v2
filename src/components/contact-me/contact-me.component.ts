ceee215c30376dbe3d15dc25b008c08d479e46aa
Akram <akrambenaoun1993@gmail.com>
2026-03-02 00:23:51 +0100

    CREATE contact-me TS file

--- src/components/contact-me/contact-me.component.ts
+++ src/components/contact-me/contact-me.component.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  FormsModule,
+  ReactiveFormsModule,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-contact-me',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './contact-me.component.html',
-  styleUrl: './contact-me.component.css'
+  styleUrls: ['./contact-me.component.css'],
 })
 export class ContactMeComponent {
+  contactForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      subject: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required],
+    });
+  }
+
+  submitForm() {
+    if (this.contactForm.valid) {
+      console.log('Form Submitted', this.contactForm.value);
+      // Here you can implement sending the form via email/api
+      this.contactForm.reset();
+      alert('Thank you! Your message has been sent.');
+    } else {
+      this.contactForm.markAllAsTouched();
+    }
+  }
 }
