a17e04585a74f6333d4d262c70e60ee5339d9591
Akram <akrambenaoun1993@gmail.com>
2026-03-02 00:59:27 +0100

    UPDATE environments files

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  EMAILJS_SERVICE_ID: '',
+  EMAILJS_TEMPLATE_ID: '',
+  EMAILJS_AUTO_REPLY_TEMPLATE_ID: '',
+  EMAILJS_PUBLIC_KEY: '',
+  GOOGLE_ANALYTICS_ID: '',
+
+  OPEN_WEATHER_API_KEY: '',
+  OPEN_WEATHER_API_URL: '',
+};
