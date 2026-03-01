e139580d616b74c6416efcdd7e0b2e2c6c114a3a
Akram <akrambenaoun1993@gmail.com>
2026-03-01 23:43:34 +0100

    create projects TS file

--- src/components/projects/projects.component.ts
+++ src/components/projects/projects.component.ts
@@ -1,11 +1,85 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.css'
+  styleUrls: ['./projects.component.css'],
 })
 export class ProjectsComponent {
+  currentIndex = 0;
+  autoSlideInterval: any;
+  startX = 0;
+  isModalOpen = false;
+  selectedProject: any = null;
 
+  projects = [
+    {
+      name: 'Project One',
+      description: 'Enterprise dashboard built with Angular & Fluent2.',
+      image: 'https://picsum.photos/1400/800?random=1',
+    },
+    {
+      name: 'Project Two',
+      description: 'Real-time chat application using Socket.IO.',
+      image: 'https://picsum.photos/1400/800?random=2',
+    },
+    {
+      name: 'Project Three',
+      description: 'Full-stack platform with Spring Boot & PostgreSQL.',
+      image: 'https://picsum.photos/1400/800?random=3',
+    },
+  ];
+
+  ngOnInit() {
+    this.startAutoSlide();
+  }
+
+  startAutoSlide() {
+    this.autoSlideInterval = setInterval(() => {
+      this.next();
+    }, 5000);
+  }
+
+  resetAutoSlide() {
+    clearInterval(this.autoSlideInterval);
+    this.startAutoSlide();
+  }
+
+  next() {
+    this.currentIndex = (this.currentIndex + 1) % this.projects.length;
+    this.resetAutoSlide();
+  }
+
+  prev() {
+    this.currentIndex =
+      (this.currentIndex - 1 + this.projects.length) % this.projects.length;
+    this.resetAutoSlide();
+  }
+
+  goTo(index: number) {
+    this.currentIndex = index;
+    this.resetAutoSlide();
+  }
+
+  openModal(project: any) {
+    this.selectedProject = project;
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
+
+  onTouchStart(event: TouchEvent) {
+    this.startX = event.touches[0].clientX;
+  }
+
+  onTouchEnd(event: TouchEvent) {
+    const endX = event.changedTouches[0].clientX;
+    if (this.startX - endX > 50) this.next();
+    if (endX - this.startX > 50) this.prev();
+  }
 }
