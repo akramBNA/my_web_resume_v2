bf5a74ab274e784f052d332d1d45d4216852b76c
Akram <akrambenaoun1993@gmail.com>
2026-03-02 00:26:22 +0100

    CREATE contact-me html file

--- src/components/contact-me/contact-me.component.ts
+++ src/components/contact-me/contact-me.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
   FormsModule,
@@ -14,6 +14,7 @@ import {
   imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './contact-me.component.html',
   styleUrls: ['./contact-me.component.css'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class ContactMeComponent {
   contactForm: FormGroup;
