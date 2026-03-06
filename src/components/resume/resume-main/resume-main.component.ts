8a845b1146336aeb96468297d24cf10d278553bf
Akram <akrambenaoun1993@gmail.com>
2026-03-06 07:39:04 +0100

    create a new resume-main component

---
+++ src/components/resume/resume-main/resume-main.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resume-main',
+  imports: [],
+  templateUrl: './resume-main.component.html',
+  styleUrl: './resume-main.component.css'
+})
+export class ResumeMainComponent {
+
+}
