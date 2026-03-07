2ddd005c629a7a73da9f4ba543e73965bca0b1d2
Akram <akrambenaoun1993@gmail.com>
2026-03-07 23:39:20 +0100

    update resume-main

--- src/components/resume/resume-main/resume-main.component.ts
+++ src/components/resume/resume-main/resume-main.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { TempResumePageComponent } from '../temp-resume-page/temp-resume-page.component';
+// import { TempResumePageComponent } from '../temp-resume-page/temp-resume-page.component';
 import { ContactDetailsComponent } from '../contact-details/contact-details.component';
 
 @Component({
   selector: 'app-resume-main',
-  imports: [TempResumePageComponent, ContactDetailsComponent],
+  imports: [ContactDetailsComponent],
   templateUrl: './resume-main.component.html',
   styleUrl: './resume-main.component.css',
 })
-export class ResumeMainComponent {
-}
+export class ResumeMainComponent {}
