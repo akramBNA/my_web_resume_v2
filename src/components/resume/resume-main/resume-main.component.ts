292e4eee88b34a214cae6bc6d90a213b92d4a9ec
Akram <akrambenaoun1993@gmail.com>
2026-03-07 23:34:23 +0100

    ADD contact-details component to resume-main

--- src/components/resume/resume-main/resume-main.component.ts
+++ src/components/resume/resume-main/resume-main.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { TempResumePageComponent } from '../temp-resume-page/temp-resume-page.component';
+import { ContactDetailsComponent } from '../contact-details/contact-details.component';
 
 @Component({
   selector: 'app-resume-main',
-  imports: [TempResumePageComponent],
+  imports: [TempResumePageComponent, ContactDetailsComponent],
   templateUrl: './resume-main.component.html',
   styleUrl: './resume-main.component.css',
 })
