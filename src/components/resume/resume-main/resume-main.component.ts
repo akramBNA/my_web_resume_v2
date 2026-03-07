0e944f7a47e1088ed83d46b7ea64706581d63858
Akram <akrambenaoun1993@gmail.com>
2026-03-07 23:29:45 +0100

    Move the resume-main component's content to a temp-resume-page

--- src/components/resume/resume-main/resume-main.component.ts
+++ src/components/resume/resume-main/resume-main.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { TempResumePageComponent } from '../temp-resume-page/temp-resume-page.component';
 
 @Component({
   selector: 'app-resume-main',
-  imports: [RouterLink],
+  imports: [RouterLink, TempResumePageComponent],
   templateUrl: './resume-main.component.html',
   styleUrl: './resume-main.component.css',
 })
 export class ResumeMainComponent {
-  goBack() {
-    window.history.back();
-  }
 }
--- src/components/resume/temp-resume-page/temp-resume-page.component.ts
+++ src/components/resume/temp-resume-page/temp-resume-page.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-temp-resume-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './temp-resume-page.component.html',
-  styleUrl: './temp-resume-page.component.css'
+  styleUrl: './temp-resume-page.component.css',
 })
 export class TempResumePageComponent {
-
+  goBack() {
+    window.history.back();
+  }
 }
