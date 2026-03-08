4e6f000fe7d90766f6beffaac6f6fb15778953d8
Akram <akrambenaoun1993@gmail.com>
2026-03-08 23:36:22 +0100

    REMOVE resume footer and navbar, becomes one unified style

--- src/components/resume/resume-main/resume-main.component.ts
+++ src/components/resume/resume-main/resume-main.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 // import { TempResumePageComponent } from '../temp-resume-page/temp-resume-page.component';
-import { ResumeNavbarComponent } from '../resume-navbar/resume-navbar.component';
+// import { ResumeNavbarComponent } from '../resume-navbar/resume-navbar.component';
 import { ContactDetailsComponent } from '../contact-details/contact-details.component';
-import { ResumeFooterComponent } from '../resume-footer/resume-footer.component';
+// import { ResumeFooterComponent } from '../resume-footer/resume-footer.component';
 
 @Component({
   selector: 'app-resume-main',
-  imports: [ResumeNavbarComponent, ContactDetailsComponent, ResumeFooterComponent],
+  imports: [ContactDetailsComponent],
   templateUrl: './resume-main.component.html',
   styleUrl: './resume-main.component.css',
 })
