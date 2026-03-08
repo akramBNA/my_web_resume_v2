64dab33b15556246a327d35a137ff512543c494e
Akram <akrambenaoun1993@gmail.com>
2026-03-09 00:19:27 +0100

    CREATE a new experience component

--- src/components/resume/resume-main/resume-main.component.ts
+++ src/components/resume/resume-main/resume-main.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 // import { TempResumePageComponent } from '../temp-resume-page/temp-resume-page.component';
 // import { ResumeNavbarComponent } from '../resume-navbar/resume-navbar.component';
 import { ContactDetailsComponent } from '../contact-details/contact-details.component';
+import { ExperienceComponent } from '../experience/experience.component';
 // import { ResumeFooterComponent } from '../resume-footer/resume-footer.component';
 
 @Component({
   selector: 'app-resume-main',
-  imports: [ContactDetailsComponent],
+  imports: [ContactDetailsComponent, ExperienceComponent],
   templateUrl: './resume-main.component.html',
   styleUrl: './resume-main.component.css',
 })
