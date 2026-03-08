9e7c094f4e1141120e1ef5d6f6680e51a7fe1bc1
Akram <akrambenaoun1993@gmail.com>
2026-03-09 00:42:42 +0100

    UPDATE resume-main page components imports

--- src/components/resume/resume-main/resume-main.component.ts
+++ src/components/resume/resume-main/resume-main.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { ContactDetailsComponent } from '../contact-details/contact-details.component';
 import { ExperienceComponent } from '../experience/experience.component';
 import { InternshipsComponent } from '../internships/internships.component';
+import { EducationComponent } from '../education/education.component';
 
 @Component({
   selector: 'app-resume-main',
-  imports: [ContactDetailsComponent, ExperienceComponent, InternshipsComponent],
+  imports: [ContactDetailsComponent, ExperienceComponent, InternshipsComponent, EducationComponent],
   templateUrl: './resume-main.component.html',
   styleUrl: './resume-main.component.css',
 })
