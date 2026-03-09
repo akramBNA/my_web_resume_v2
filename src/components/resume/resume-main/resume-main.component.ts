6e6d5ef93f3164df86c1573feb784f4fa4c25306
Akram <akrambenaoun1993@gmail.com>
2026-03-09 01:30:09 +0100

    ADD new component imports in resume-main

--- src/components/resume/resume-main/resume-main.component.ts
+++ src/components/resume/resume-main/resume-main.component.ts
@@ -5,6 +5,7 @@ import { InternshipsComponent } from '../internships/internships.component';
 import { EducationComponent } from '../education/education.component';
 import { SkillsComponent } from '../skills/skills.component';
 import { CertificatesComponent } from '../certificates/certificates.component';
+import { LanguagesComponent } from '../languages/languages.component';
 
 @Component({
   selector: 'app-resume-main',
@@ -15,6 +16,7 @@ import { CertificatesComponent } from '../certificates/certificates.component';
     EducationComponent,
     SkillsComponent,
     CertificatesComponent,
+    LanguagesComponent,
   ],
   templateUrl: './resume-main.component.html',
   styleUrl: './resume-main.component.css',
